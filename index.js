2eec049c69bcfbb7f0e4d2bd22a4410fa65802e6
DIANA IRENE PARK <49377760@ort>
2026-03-20 10:39:02 -0300

    inicio ej1

---
+++ index.js
@@ -0,0 +1,14 @@
+// #region EJ1
+import { readFile } from './src/modules/file.js';
+
+console.log('Leyendo el archivo...');
+readFile('./productos.json').then((data) => {
+  console.log(data);
+});
+// #endregion
+
+// #region EJ2
+// #endregion
+
+// #region EJ3
+// #endregion
---
+++ src/modules/file.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+export function readFile(filePath) {
+  return fs.promises.readFile(filePath, 'utf-8');
+}
+
